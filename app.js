5d7bf18bf6f23eb6022832f006740c89fb3b76a2
antikun <121240743+antikun@users.noreply.github.com>
2023-01-25 23:07:08 +0200

    Add logic for ending the game

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const btnOne = document.querySelector("#btnOne");
 const btnTwo = document.querySelector("#btnTwo");
 const score = document.querySelector("#score");
-const roundsTotal = document.querySelector("#rounds");
+const rounds = document.querySelector("#rounds");
 
 let pointsOne = 0;
 let pointsTwo = 0;
+let totalPoints = pointsOne + pointsTwo;
 
 btnOne.addEventListener("click", (e) => {
     e.preventDefault();
@@ -12,5 +13,16 @@ btnOne.addEventListener("click", (e) => {
     score.textContent = `${pointsOne} to ${pointsTwo}`;
 });
 
+btnTwo.addEventListener("click", (e) => {
+    e.preventDefault();
+    pointsTwo++
+    score.textContent = `${pointsOne} to ${pointsTwo}`;
+});
 
-
+rounds.addEventListener("change", () => {
+    let totalRounds = rounds.value;
+    if (totalRounds === totalPoints) {
+        btnOne.setAttribute("disabled", "");
+        btnTwo.setAttribute("disabled", "");
+    }
+})
