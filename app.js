c8a82f2e0da263aade91253168a3e82cd08f7d78
antikun <121240743+antikun@users.noreply.github.com>
2023-01-26 00:49:55 +0200

    Finish up JS logic
    Removed the form element from the HTML file as it actually made no sense
    having it in the first place, which simplified things a litlle bit.
    
    Added two if conditionals per button because if...else was letting me
    click the button once more before disabling both buttons. This led to
    some unexpected results - one player could have scored 5 points but since
    the button only blurred ON the sixth click, the second player could still
    score points.

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const btnOne = document.querySelector("#btnOne");
 const btnTwo = document.querySelector("#btnTwo");
-const score = document.querySelector("#score");
-const rounds = document.querySelector("#rounds");
+const pointsOne = document.querySelector(".pointsOne");
+const pointsTwo = document.querySelector(".pointsTwo");
+const winningScore = document.querySelector("#winningScore");
+const resetBtn = document.querySelector("#reset");
 
-let pointsOne = 0;
-let pointsTwo = 0;
-let totalPoints = pointsOne + pointsTwo;
+let clicksOne = 0;
+let clicksTwo = 0;
 
-btnOne.addEventListener("click", (e) => {
-    e.preventDefault();
-    pointsOne++
-    score.textContent = `${pointsOne} to ${pointsTwo}`;
-});
-
-btnTwo.addEventListener("click", (e) => {
-    e.preventDefault();
-    pointsTwo++
-    score.textContent = `${pointsOne} to ${pointsTwo}`;
-});
-
-rounds.addEventListener("change", () => {
-    let totalRounds = rounds.value;
-    if (totalRounds === totalPoints) {
+btnOne.addEventListener("mousedown", () => {
+    if (clicksOne < winningScore.value) {
+        clicksOne += 1;
+        pointsOne.textContent = clicksOne;
+    }
+    if (clicksOne == winningScore.value) {
         btnOne.setAttribute("disabled", "");
         btnTwo.setAttribute("disabled", "");
     }
-})
+});
+
+btnTwo.addEventListener("mousedown", () => {
+    if (clicksTwo < winningScore.value) {
+        clicksTwo++;
+        pointsTwo.textContent = clicksTwo;
+    }
+    if (clicksTwo == winningScore.value) {
+        btnOne.setAttribute("disabled", "");
+        btnTwo.setAttribute("disabled", "");
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+    clicksOne = 0;
+    clicksTwo = 0;
+    pointsOne.textContent = clicksOne;
+    pointsTwo.textContent = clicksTwo;
+    btnOne.toggleAttribute("disabled");
+    btnTwo.toggleAttribute("disabled");
+});
